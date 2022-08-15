ad51d3c42298f90239ce552b995bf0823b385232
Joseph Walsh <josephwalsh@arista.com>
2022-08-15 20:44:19 +0100

    login

--- configLogin.php
+++ configLogin.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
-$dbname = "Project";
+$dbname = "project";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
--- configMedals.php
+++ configMedals.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "root";
-$dbname = "Medals";
+$dbname = "medals";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
